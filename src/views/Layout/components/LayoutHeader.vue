96c90192970b70b27610785f1cc7e68e9074ee9f
jiege <1768669274@qq.com>
2024-01-12 19:37:03 +0800

    pinia优化导航数据

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,19 +1,6 @@
 <script setup>
-import { getCategoryAPI } from "@/apis/layout";
-import { onMounted, ref } from "vue";
-
-const categoryList = ref([]);
-const getCategory = async () => {
-  try {
-    const res = await getCategoryAPI();
-    categoryList.value = res.result;
-  } catch (error) {
-    console.log(err);
-  }
-};
-onMounted(() => {
-  getCategory();
-});
+import { useCategoryStore } from "@/stores/category";
+const categoryStore = useCategoryStore();
 </script>
 
 <template>
@@ -23,10 +10,12 @@ onMounted(() => {
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home">
+          <RouterLink to="/">首页</RouterLink>
+        </li>
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink to="/">{{ item.name }}</RouterLink>
         </li>
-
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>
