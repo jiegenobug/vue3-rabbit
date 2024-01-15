ce42a7dade1078cfccc2307f928a7ef0ae4e8afc
jiege <1768669274@qq.com>
2024-01-15 11:18:11 +0800

    图片懒加载自定义指令实现

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import '@/styles/common.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// 检测元素是否进入视口区域
+import { useIntersectionObserver } from '@vueuse/core'
+
 import App from './App.vue'
 import router from './router'
 
@@ -12,3 +15,17 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义自定义指令图片懒加载
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    const { stop } = useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          el.src = binding.value
+        }
+      },
+    )
+  }
+})
